5e6e77cf73551fe37d9b722535c2e2758755680a
Wendel Harrison <wendel.wdhr@gmail.com>
2024-03-25 14:42:44 +0000

    Teste chamando api

--- server-intro.js
+++ server-intro.js
@@ -1,43 +1,32 @@
+const axios = require('axios');
 const { ChannelEngine } = require('eyevinn-channel-engine');
 
+const apiUrl = "http://201.17.21.186:3000/assets"
 class MyAssetManager {
     constructor() {
         this.assets = {
-            "Canal bob Esponja": [
-                {
-                    id: "e62ae11e-eee0-4372-812d-90730241831b",
-                    title: "Trailer 1",
-                    uri: "http://getcdn.nowonline.com.br/Content/h3p5/VOD/15596/16874/3cb21f12-21ad-4c14-bdeb-6be1605518a0/f4d191c5-2938-9a89-b0bb-479d2bc94678/index.m3u8"
-                },
-                {
-                    id: "3a65e827-8c75-4e7b-90d5-15f797ed1646",
-                    title: "Anuncio 1",
-                    uri: "http://getcdnlab.clarocdn.com.br/Content/h3p0/VOD/3142/13771/32fa407e-fd9c-4322-9de2-ddf22b7f4198/2ae1bdcf-be7c-fdbb-5d93-276b0fae3648/index.m3u8"
-                },
-                {
-                    id: "dd21b69f-8096-4ee5-a899-9cdabb9371b4",
-                    title: "Trailer 2",
-                    uri: "http://getcdn.nowonline.com.br/Content/h3p5/VOD/2896/8491/189b8c92-c07b-418b-822d-3d3cbdf8fa83/f4d191c5-2938-9a89-b0bb-479d2bc94678/index.m3u8"
-                },
-                {
-                    id: "dd21b69f-8096-4ee5-a899-9cdabb9371b4",
-                    title: "Anuncio 2",
-                    uri: "http://getcdnlab.clarocdn.com.br/Content/h3p0/VOD/3539/13241/a4a08b07-61e5-4850-8866-b2e07ac77030/f4d191c5-2938-9a89-b0bb-479d2bc94678/index.m3u8"
-                },
-                {
-                    id: "dd21b69f-8096-4ee5-a899-9cdabb9371b4",
-                    title: "Trailer 3",
-                    uri: "http://getcdn.nowonline.com.br/Content/HLS_2/VOD/trailer/62/27/5b5e930b-8da9-453e-a3a2-44b0e3a56227/index.m3u8"
-                },
-                {
-                    id: "dd21b69f-8096-4ee5-a899-9cdabb9371b4",
-                    title: "Trailer 3",
-                    uri: "http://getcdn.nowonline.com.br/Content/HLS_2/VOD/trailer/62/27/5b5e930b-8da9-453e-a3a2-44b0e3a56227/index.m3u8"
-                }
+            "myfirstchannel": [
+                
             ]
         };
         this.currentIndex = 0;
+        async function getAssets() {
+            try {
+                const response = await axios.get(apiUrl);
+                // Verifica se a resposta possui dados
+                if (response.data && Array.isArray(response.data)) {
+                    myfirstchannel.push(...response.data);
+                    console.log('Dados recebidos e adicionados em myfirstchannel:', myfirstchannel);
+                } else {
+                    console.log('A resposta da API não possui dados válidos.');
+                }
+            } catch (error) {
+                console.error('Erro ao fazer a requisição:', error);
+            }
+        }
+        getAssets();
     }
+
 
     async getNextVod(vodRequest) {
         const assets = this.assets["myfirstchannel"];
@@ -56,6 +45,8 @@ class MyChannelManager {
         ];
     }
 }
+
+
 
 const myAssetManager = new MyAssetManager();
 const myChannelManager = new MyChannelManager();
